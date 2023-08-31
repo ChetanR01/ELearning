38505bca568f784f6df64d457717f7f46762c17f
ChetanR01 <chetan0101career@gmail.com>
2023-08-31 18:18:46 +0530

    first commit

---
+++ App/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name="Index"),
+    path('about',views.about,name="about"),
+    path('courses',views.courses,name="courses"),
+    path('teachers',views.teachers,name="teachers"),
+    path('blog',views.blog,name="blog"),
+    path('contact',views.contact,name="contact"),
+    path('single',views.single,name="single"),
+]
---
+++ App/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render , HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html",{})
+
+def about(request):
+    return render(request,"about.html",{})
+
+def courses(request):
+    return render(request,"courses.html",{})
+
+def teachers(request):
+    return render(request,"teachers.html",{})
+
+def blog(request):
+    return render(request,"blog.html",{})
+
+def contact(request):
+    return render(request,"contact.html",{})
+
+def single(request):
+    return render(request,"single.html",{})
